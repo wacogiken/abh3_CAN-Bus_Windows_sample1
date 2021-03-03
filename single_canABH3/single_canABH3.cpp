97f226700011292286177f197ce55846115efa0f
yo0043 <yo0043@YO0043W10>
2021-03-03 10:14:40 +0900

    Document update

--- single_canABH3/single_canABH3.cpp
+++ single_canABH3/single_canABH3.cpp
@@ -1,3 +1,49 @@
+/*
+ ******************************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2021, Waco Giken Co., Ltd.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/o2r other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Waco Giken nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************
+
+******************************************************************************
+@file           single_canABH3.cpp
+******************************************************************************
+@brief          ABH3用CAN サンプルソフト(アプリケーション)
+******************************************************************************
+@date           2021.03.03
+@author         Y.Ogawa
+@note			1st release
+@note           This sourcecode is based by "single_canABH3.c"
+******************************************************************************
+*/
 
 #include "stdafx.h"
 #include <conio.h>
@@ -21,6 +67,10 @@
 #define	BAUDRATE	(500)	//CAN通信に使用するボーレート[kbps]
 #define BRDGRP		(5)		//ブロードキャストグループ番号
 
+//DLLの関数を簡単に使う為の構造体
+CA3_FUNCLIST funcList;
+
+//
 int main()
 	{
 	//戻り値
@@ -42,7 +92,6 @@ int main()
 		}
 
 	//DLLの関数を楽に扱う為、用意された構造体にエントリを一括設定
-	CA3_FUNCLIST funcList;
 	GetFunctions(hDLL,&funcList);
 
 	//========================================
